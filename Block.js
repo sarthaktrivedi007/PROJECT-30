3147542be81a8298a68281c8f7337bde8760b8d7
sarthaktrivedi007 <83532825+sarthaktrivedi007@users.noreply.github.com>
2021-06-09 20:53:48 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,41 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("block.png")
+      World.add(world, this.body);
+      this.visiblity = 255
+
+
+    }
+
+
+    display(){
+      if(this.body.speed>3){
+        World.remove(world,this.body)
+        push()
+        this.visiblity = this.visiblity - 5;
+        tint(255 , this.visiblity)
+        imageMode(CENTER);
+        image(this.image,this.body.position.x,this.body.position.y,this.width, this.height);
+        pop()
+        
+      }
+
+      else{
+        var pos= this.body.position;
+        push()
+      imageMode(CENTER);
+      image(this.image,pos.x,pos.y,this.width, this.height);
+      pop()
+       
+        
+      }
+
+    }
+}
